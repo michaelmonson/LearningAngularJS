585e22c04180cf595ed5a5c7f8614b51025c4f23
Michael Monson <michaelmonson@ldschurch.org>
2014-03-21 01:01:41 -0600

    Pushing out newest changes.  Adding my second expirment for data input binding...

---
+++ js/commonCtrl.js
@@ -0,0 +1,10 @@
+
+/* Controller file */
+
+app.controller("FirstCtrl", function($scope) {
+	$scope.understanding = "Look closely, my friend, and you will see how the scope works!";
+});
+
+app.controller("SecondCtrl", function($scope) {
+	$scope.inputValue = "";
+});
